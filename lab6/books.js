1653135cec33d311192103b658356b94029db6cf
Алёшкина Варя <hihikok678@mail.ru>
2025-05-16 07:50:16 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+                {
+                    author: 'Ozon671games',
+                    name: 'Тихий ден',
+                    publicationYear: 2022,
+                    price: 500,
+                    bestseller: false,
+                    available: true
+                },
+                {
+                    author: 'Харлан Эллисон',
+                    name: 'У меня нет рта, но я должен кричать',
+                    publicationYear: 1967,
+                    price: 700,
+                    bestseller: true,
+                    available: true
+                },
+                {
+                    author: 'Томас Харрис',
+                    name: 'Молчание ягнят',
+                    publicationYear: 1988,
+                    price: 600,
+                    bestseller: true,
+                    available: false
+                },
+                {
+                    author: 'Джордж Оруэлл',
+                    name: '1984',
+                    publicationYear: 1949,
+                    price: 450,
+                    bestseller: true,
+                    available: true
+                },
+                {
+                    author: 'Рэй Брэдбери',
+                    name: '451 градус по Фаренгейту',
+                    publicationYear: 1953,
+                    price: 550,
+                    bestseller: true,
+                    available: true
+                },
+                {
+                    author: 'Габриэль Гарсиа Маркес',
+                    name: 'Сто лет одиночества',
+                    publicationYear: 1967,
+                    price: 800,
+                    bestseller: true,
+                    available: false
+                },
+                {
+                    author: 'Фрэнк Герберт',
+                    name: 'Дюна',
+                    publicationYear: 1965,
+                    price: 900,
+                    bestseller: true,
+                    available: true
+                },
+                {
+                    author: 'Дж. Р. Р. Толкин',
+                    name: 'Властелин колец',
+                    publicationYear: 1954,
+                    price: 1200,
+                    bestseller: true,
+                    available: true
+                },
+                {
+                    author: 'Антуан де Сент-Экзюпери',
+                    name: 'Маленький принц',
+                    publicationYear: 1943,
+                    price: 350,
+                    bestseller: false,
+                    available: true
+                },
+                {
+                    author: 'Лев Толстой',
+                    name: 'Война и мир',
+                    publicationYear: 1869,
+                    price: 1100,
+                    bestseller: true,
+                    available: true
+                }
+            ];
